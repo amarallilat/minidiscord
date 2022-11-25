5cae4f3c9c6e04ed9a4e3865a923628c54014b6d
amarallilat <allilat.amar@gmail.com>
2022-11-25 21:25:06 +0100

    sql

--- messagerie.sql
+++ messagerie.sql
@@ -24,6 +24,30 @@ CREATE DATABASE /*!32312 IF NOT EXISTS*/ `messagerie` /*!40100 DEFAULT CHARACTER
 USE `messagerie`;
 
 --
+-- Table structure for table `groupe`
+--
+
+DROP TABLE IF EXISTS `groupe`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `groupe` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(300) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `groupe`
+--
+
+LOCK TABLES `groupe` WRITE;
+/*!40000 ALTER TABLE `groupe` DISABLE KEYS */;
+INSERT INTO `groupe` VALUES (1,'groupe');
+/*!40000 ALTER TABLE `groupe` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `message`
 --
 
@@ -36,12 +60,13 @@ CREATE TABLE `message` (
   `id_expediteur` int(11) NOT NULL,
   `id_recepteur` int(11) DEFAULT NULL,
   `heure` datetime NOT NULL DEFAULT current_timestamp(),
+  `id_groupe` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `message_FK` (`id_expediteur`),
   KEY `message_FK_1` (`id_recepteur`),
   CONSTRAINT `message_FK` FOREIGN KEY (`id_expediteur`) REFERENCES `utilisateur` (`id`),
   CONSTRAINT `message_FK_1` FOREIGN KEY (`id_recepteur`) REFERENCES `utilisateur` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=121 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=146 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -50,7 +75,7 @@ CREATE TABLE `message` (
 
 LOCK TABLES `message` WRITE;
 /*!40000 ALTER TABLE `message` DISABLE KEYS */;
-INSERT INTO `message` VALUES (1,'Bonjour amar, comment ça va?',1,2,'2022-11-02 16:58:04'),(2,'cc',3,1,'2022-11-02 16:59:50'),(3,'SAmoussa, zakaria me reponds pas?',3,4,'2022-11-02 17:00:55'),(4,'Je ne sais pas',4,3,'2022-11-02 17:01:22'),(5,'fatma elle deuh',1,5,'2022-11-02 17:16:38'),(9,'bonjour a tous',2,NULL,'2022-11-02 17:44:43'),(10,'bonjour a tous aaaaaaa',1,NULL,'2022-11-07 15:34:46'),(11,'bnjr',3,NULL,'2022-11-07 15:46:24'),(12,'Bonjour a tout le monde',2,NULL,'2022-11-09 13:10:12'),(15,'bonjour fatma',2,3,'2022-11-02 17:44:43'),(24,'fatma',2,NULL,'2022-11-15 13:11:48'),(48,'slt',3,NULL,'2022-11-16 15:03:10'),(50,'buenos dias',3,NULL,'2022-11-16 15:05:25'),(51,'me hablo espagnol',3,NULL,'2022-11-16 15:06:29'),(55,'booooooooooooooooooooonjouuuuuuuuuuuuuuuuuuuuuuuuur',3,NULL,'2022-11-16 15:11:18'),(56,'auuuuuuuuuu reeeeeeeeeeeeeevoooooiiiiiiiiiiirrr',3,NULL,'2022-11-16 15:11:28'),(59,'&#128528;',2,NULL,'2022-11-16 15:17:18'),(60,'&#128528;',2,NULL,'2022-11-16 15:17:32'),(61,'&#128528',2,NULL,'2022-11-16 15:17:57'),(98,'vu...',2,3,'2022-11-17 15:32:43'),(99,'repon un peu',2,3,'2022-11-17 15:33:03');
+INSERT INTO `message` VALUES (1,'Bonjour amar, comment ça va?',1,2,'2022-11-02 16:58:04',NULL),(2,'cc',3,1,'2022-11-02 16:59:50',NULL),(3,'SAmoussa, zakaria me reponds pas?',3,4,'2022-11-02 17:00:55',NULL),(4,'Je ne sais pas',4,3,'2022-11-02 17:01:22',NULL),(5,'fatma elle deuh',1,5,'2022-11-02 17:16:38',NULL),(9,'bonjour a tous',2,NULL,'2022-11-02 17:44:43',NULL),(10,'bonjour a tous aaaaaaa',1,NULL,'2022-11-07 15:34:46',NULL),(11,'bnjr',3,NULL,'2022-11-07 15:46:24',NULL),(12,'Bonjour a tout le monde',2,NULL,'2022-11-09 13:10:12',NULL),(15,'bonjour fatma',2,3,'2022-11-02 17:44:43',NULL),(24,'fatma',2,NULL,'2022-11-15 13:11:48',NULL),(48,'slt',3,NULL,'2022-11-16 15:03:10',NULL),(50,'buenos dias',3,NULL,'2022-11-16 15:05:25',NULL),(51,'me hablo espagnol',3,NULL,'2022-11-16 15:06:29',NULL),(55,'booooooooooooooooooooonjouuuuuuuuuuuuuuuuuuuuuuuuur',3,NULL,'2022-11-16 15:11:18',NULL),(56,'auuuuuuuuuu reeeeeeeeeeeeeevoooooiiiiiiiiiiirrr',3,NULL,'2022-11-16 15:11:28',NULL),(59,'&#128528;',2,NULL,'2022-11-16 15:17:18',NULL),(60,'&#128528;',2,NULL,'2022-11-16 15:17:32',NULL),(61,'&#128528',2,NULL,'2022-11-16 15:17:57',NULL),(98,'vu...',2,3,'2022-11-17 15:32:43',NULL),(99,'repon un peu',2,3,'2022-11-17 15:33:03',NULL),(121,'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',3,NULL,'2022-11-18 13:11:11',NULL),(122,'bonjour',2,NULL,'2022-11-18 14:30:48',NULL),(123,'bonjour',2,5,'2022-11-18 14:31:16',NULL),(124,'bonjour',5,2,'2022-11-18 14:31:44',NULL),(127,'jjdjdjh',1,1,'2022-11-18 14:53:41',NULL),(128,'BONJOUR',5,NULL,'2022-11-18 15:06:21',NULL),(129,'jeeçufezç',2,NULL,'2022-11-18 15:06:39',NULL),(130,'Bonjour oui ça va et toi',2,1,'2022-11-18 15:07:03',NULL),(131,'yszydz',2,1,'2022-11-18 15:09:27',NULL),(132,'ayydydy',2,1,'2022-11-18 15:09:40',NULL),(133,'è_zdfay_çdèza',2,1,'2022-11-18 15:13:28',NULL),(134,'aaaaaaaaaaaa',2,1,'2022-11-18 15:14:14',NULL),(135,'aaaaaaaaaaaaaaa',2,1,'2022-11-18 15:14:15',NULL),(136,'aaaaaaaaaaaaaaa',2,1,'2022-11-18 15:14:16',NULL),(137,'aaaaaaaaaaaaaaaaaaaaa',2,1,'2022-11-18 15:14:19',NULL),(138,'aaaaaaaaaaaaaaaaaa',2,1,'2022-11-18 15:14:20',NULL),(139,'gian',2,NULL,'2022-11-18 15:19:38',NULL),(140,'aahahaha',2,3,'2022-11-18 15:19:48',NULL),(141,'dhdhaa',3,2,'2022-11-18 15:20:29',NULL),(142,'ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff',2,NULL,'2022-11-21 20:30:33',NULL),(143,'fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff fffffffffffffffffffffffffffffffffffffffffffffffff',2,NULL,'2022-11-21 20:32:21',NULL),(144,'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui voluptate tempore ratione? Soluta delectus voluptatem fugiat et vitae alias natus quisquam dignissimos mollitia inventore, nostrum, aliquam consectetur doloremque dolor repudiandae.',2,NULL,'2022-11-21 20:34:33',NULL),(145,'t ou',3,6,'2022-11-22 15:50:46',NULL);
 /*!40000 ALTER TABLE `message` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -82,6 +107,32 @@ LOCK TABLES `utilisateur` WRITE;
 INSERT INTO `utilisateur` VALUES (1,'Hanafi','Zakaria','zakaria@hanafi.fr','123','zakarialogo.gif','zakaria213'),(2,'Allilat','Amar','amar@allilat.fr','123','amar.jpeg','amar144'),(3,'بودبوس','فاطمة','fatma@bou.fr','123','fatma.jpg','fatma216'),(4,'Soumsa','Kwami','soumsa@kwami.com','123','index.jpeg','kwami228'),(5,'Illoul','Amine','amine@illoul.fr','123','amine.jpeg','amine33'),(6,'Idrismoussa','Abdousalam','idrismoussa@abdousalam.fr','123','abdou.jpg','saviola');
 /*!40000 ALTER TABLE `utilisateur` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `utilisateur_groupe`
+--
+
+DROP TABLE IF EXISTS `utilisateur_groupe`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `utilisateur_groupe` (
+  `id_utilisateur` int(11) NOT NULL,
+  `id_groupe` int(11) NOT NULL,
+  KEY `utilisateur_groupe_FK` (`id_utilisateur`),
+  KEY `utilisateur_groupe_FK_1` (`id_groupe`),
+  CONSTRAINT `utilisateur_groupe_FK` FOREIGN KEY (`id_utilisateur`) REFERENCES `utilisateur` (`id`),
+  CONSTRAINT `utilisateur_groupe_FK_1` FOREIGN KEY (`id_groupe`) REFERENCES `groupe` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `utilisateur_groupe`
+--
+
+LOCK TABLES `utilisateur_groupe` WRITE;
+/*!40000 ALTER TABLE `utilisateur_groupe` DISABLE KEYS */;
+/*!40000 ALTER TABLE `utilisateur_groupe` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -92,4 +143,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-11-17 22:22:31
+-- Dump completed on 2022-11-25 14:26:17
